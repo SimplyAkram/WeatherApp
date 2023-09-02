6c38936e78375ecea4bc2e4243e1a5a2f7807bc0
Akram Ali <81180304+SimplyAkram@users.noreply.github.com>
2023-09-02 00:11:49 -0500

    Add files via upload

---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Akram Ali on 3/27/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    
+}
+
+
+
+
+
+
+
+
+
